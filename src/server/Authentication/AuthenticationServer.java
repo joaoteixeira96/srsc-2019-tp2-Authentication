8d64ac3281e23d1b88f8f99f75bbcc677eb8eecf
joaoteixeira96 <ja.teixeira@campus.fct.unl.pt>
2019-05-29 15:39:55 +0100

    updated authentication

--- src/server/Authentication/AuthenticationServer.java
+++ src/server/Authentication/AuthenticationServer.java
@@ -1,25 +1,28 @@
 package server.Authentication;
 
-		import TLS_Utils.TLSConfiguration;
-		import TLS_Utils.TLSServerCreate;
+import TLS_Utils.TLSConfiguration;
+import TLS_Utils.TLSServerCreate;
 
-		import java.io.*;
-		import java.util.Properties;
+import java.io.*;
+import java.util.Properties;
 
-		import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocket;
 
 public class AuthenticationServer {
 
 	private static final int PORT = 8083;
 
 	public static void main(String[] args) throws Exception {
-		mainCycle();
+		SSLSocket socket = (SSLSocket) TLSServerCreate.createSSLServer(getConfiguration(),"password".toCharArray(),PORT).accept();
+		
+		mainCycle(socket);
 	}
 	
-	private static void mainCycle() throws Exception{
+	private static void mainCycle(SSLSocket socket) throws Exception{
+		PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		while(true)
-			receiveCommunication((SSLSocket) TLSServerCreate.createSSLServer(getConfiguration(),"password".toCharArray(),PORT).accept());
-	
+			receiveCommunication(socket,in,out);
 	}
 
 	private static TLSConfiguration getConfiguration() throws IOException {
@@ -29,18 +32,16 @@ public class AuthenticationServer {
 		return new TLSConfiguration(properties.getProperty("TLS-PROT-ENF"),properties.getProperty("TLS-AUTH"),properties.getProperty("CIPHERSUITES").split(";"),"authentication.jks","authenticationTruststore.jks");
 	}
 	
-    private static void sendMessage(String message, SSLSocket socket) throws IOException {
-        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+    private static void sendMessage(String message, SSLSocket socket, PrintWriter out) throws IOException {
+    	System.out.println("Message sent :" + message);
         out.println(message);
-        out.println();
         out.flush();
     }
     
-	private static void receiveCommunication(SSLSocket socket) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		sendMessage(MainDispatcherHandler.login(in.readLine()), socket);
-
+	private static void receiveCommunication(SSLSocket socket, BufferedReader in, PrintWriter out) throws Exception {
+		String message = in.readLine();
+		System.out.println("Received message :" +message);
+		sendMessage(MainDispatcherHandler.login(message), socket,out);
 	}
-
 
 }
